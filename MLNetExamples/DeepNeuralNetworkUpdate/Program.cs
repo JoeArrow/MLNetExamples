ad895fa5f9af4836009d041fdcfad6a74d482a6c
JoeArrow <joearrow1964@gmail.com>
2024-05-10 13:18:02 -0400

    Update is working

--- MLNetExamples/DeepNeuralNetworkUpdate/Program.cs
+++ MLNetExamples/DeepNeuralNetworkUpdate/Program.cs
@@ -1,9 +1,9 @@
-﻿using Microsoft.ML;
-using Microsoft.ML.Data;
-using Microsoft.ML.Vision;
-using System;
+﻿using System;
 using System.IO;
 using System.Linq;
+
+using Microsoft.ML;
+using Microsoft.ML.Vision;
 
 namespace DeepNeuralNetworkUpdate
 {
@@ -11,6 +11,12 @@ namespace DeepNeuralNetworkUpdate
     {
         static void Main(string[] args)
         {
+            var cr = Environment.NewLine;
+            var modelPath = "./dnn_model.zip";
+            ITransformer model;
+
+            var context = new MLContext();
+
             var imagesFolder = Path.Combine(Environment.CurrentDirectory, "..", "..", "..", "images");
 
             var files = Directory.GetFiles(imagesFolder, "*", SearchOption.AllDirectories);
@@ -21,19 +27,19 @@ namespace DeepNeuralNetworkUpdate
                 Label = Directory.GetParent(file).Name
             });
 
-            var context = new MLContext();
-
             var imageData = context.Data.LoadFromEnumerable(images);
             var imageDataShuffled = context.Data.ShuffleRows(imageData);
 
             var testTrainData = context.Data.TrainTestSplit(imageDataShuffled, testFraction: 0.2);
 
-            var validationData = context.Transforms.Conversion.MapValueToKey("LabelKey", "Label", keyOrdinality: Microsoft.ML.Transforms.ValueToKeyMappingEstimator.KeyOrdinality.ByValue)
+            var validationData =
+                context.Transforms.Conversion.MapValueToKey("LabelKey", "Label", keyOrdinality: Microsoft.ML.Transforms.ValueToKeyMappingEstimator.KeyOrdinality.ByValue)
                 .Append(context.Transforms.LoadRawImageBytes("Image", imagesFolder, "ImagePath"))
                 .Fit(testTrainData.TestSet)
                 .Transform(testTrainData.TestSet);
 
-            var imagesPipeline = context.Transforms.Conversion.MapValueToKey("LabelKey", "Label", keyOrdinality: Microsoft.ML.Transforms.ValueToKeyMappingEstimator.KeyOrdinality.ByValue)
+            var imagesPipeline =
+                context.Transforms.Conversion.MapValueToKey("LabelKey", "Label", keyOrdinality: Microsoft.ML.Transforms.ValueToKeyMappingEstimator.KeyOrdinality.ByValue)
                 .Append(context.Transforms.LoadRawImageBytes("Image", imagesFolder, "ImagePath"));
 
             var imageDataModel = imagesPipeline.Fit(testTrainData.TrainSet);
@@ -54,12 +60,12 @@ namespace DeepNeuralNetworkUpdate
             var pipeline = context.MulticlassClassification.Trainers.ImageClassification(options)
                 .Append(context.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
 
-            var model = pipeline.Fit(imageDataView);
+            Console.WriteLine($"Training Model...{cr}");
+
+            model = pipeline.Fit(imageDataView);
 
             var predictionEngine = context.Model.CreatePredictionEngine<ImageModelInput, ImagePrediction>(model);
-
             var testImagesFolder = Path.Combine(Environment.CurrentDirectory, "..", "..", "..", "test");
-
             var testFiles = Directory.GetFiles(testImagesFolder, "*", SearchOption.AllDirectories);
 
             var testImages = testFiles.Select(file => new ImageModelInput
@@ -67,24 +73,22 @@ namespace DeepNeuralNetworkUpdate
                 ImagePath = file
             });
 
-            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine($"{cr}");
 
             var testImagesData = context.Data.LoadFromEnumerable(testImages);
-
             var testImageDataView = imagesPipeline.Fit(testImagesData).Transform(testImagesData);
-
             var predictions = model.Transform(testImageDataView);
-
             var testPredictions = context.Data.CreateEnumerable<ImagePrediction>(predictions, reuseRowObject: false);
 
-            foreach (var prediction in testPredictions)
+            foreach(var prediction in testPredictions)
             {
                 var labelIndex = prediction.PredictedLabel;
-
                 Console.WriteLine($"Image: {Path.GetFileName(prediction.ImagePath)}, Predicted Label: {prediction.PredictedLabel}");
             }
 
-            context.Model.Save(model, imageData.Schema, "./dnn_model.zip");
+            context.Model.Save(model, imageData.Schema, modelPath);
+
+            Console.WriteLine($"{cr}Model Saved...{cr}{modelPath}{cr}");
         }
     }
 }
