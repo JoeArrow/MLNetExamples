56b9814cbd120d18a1a3e4dd5c599fa2a5c4c429
JoeArrow <joearrow1964@gmail.com>
2024-05-10 14:41:28 -0400

    Altered the method of prediction

--- MLNetExamples/DeepNeuralNetworkUpdate/Program.cs
+++ MLNetExamples/DeepNeuralNetworkUpdate/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 
@@ -12,83 +13,100 @@ namespace DeepNeuralNetworkUpdate
         static void Main(string[] args)
         {
             var cr = Environment.NewLine;
-            var modelPath = "./dnn_model.zip";
+            var modelPath = "./dnn_model.mdl";
             ITransformer model;
 
             var context = new MLContext();
 
-            var imagesFolder = Path.Combine(Environment.CurrentDirectory, "..", "..", "..", "images");
-
-            var files = Directory.GetFiles(imagesFolder, "*", SearchOption.AllDirectories);
-
-            var images = files.Select(file => new ImageData
+            if(File.Exists(modelPath))
             {
-                ImagePath = file,
-                Label = Directory.GetParent(file).Name
-            });
-
-            var imageData = context.Data.LoadFromEnumerable(images);
-            var imageDataShuffled = context.Data.ShuffleRows(imageData);
-
-            var testTrainData = context.Data.TrainTestSplit(imageDataShuffled, testFraction: 0.2);
-
-            var validationData =
-                context.Transforms.Conversion.MapValueToKey("LabelKey", "Label", keyOrdinality: Microsoft.ML.Transforms.ValueToKeyMappingEstimator.KeyOrdinality.ByValue)
-                .Append(context.Transforms.LoadRawImageBytes("Image", imagesFolder, "ImagePath"))
-                .Fit(testTrainData.TestSet)
-                .Transform(testTrainData.TestSet);
-
-            var imagesPipeline =
-                context.Transforms.Conversion.MapValueToKey("LabelKey", "Label", keyOrdinality: Microsoft.ML.Transforms.ValueToKeyMappingEstimator.KeyOrdinality.ByValue)
-                .Append(context.Transforms.LoadRawImageBytes("Image", imagesFolder, "ImagePath"));
-
-            var imageDataModel = imagesPipeline.Fit(testTrainData.TrainSet);
-
-            var imageDataView = imageDataModel.Transform(testTrainData.TrainSet);
-
-            var options = new ImageClassificationTrainer.Options()
+                Console.WriteLine($"The model has already been trained...{cr}{cr}");
+                model = context.Model.Load(modelPath, out DataViewSchema inputSchema);
+            }
+            else
             {
-                Arch = ImageClassificationTrainer.Architecture.ResnetV250,
-                Epoch = 100,
-                BatchSize = 20,
-                LearningRate = 0.01f,
-                LabelColumnName = "LabelKey",
-                FeatureColumnName = "Image",
-                ValidationSet = validationData
-            };
+                var imagesFolder = Path.Combine(Environment.CurrentDirectory, "..", "..", "..", "images");
+                var files = Directory.GetFiles(imagesFolder, "*", SearchOption.AllDirectories);
 
-            var pipeline = context.MulticlassClassification.Trainers.ImageClassification(options)
-                .Append(context.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
+                var images = files.Select(file => new ImageData
+                {
+                    ImagePath = file,
+                    Label = Directory.GetParent(file).Name
+                });
 
-            Console.WriteLine($"Training Model...{cr}");
+                var imageData = context.Data.LoadFromEnumerable(images);
+                var imageDataShuffled = context.Data.ShuffleRows(imageData);
+                var dataSets = context.Data.TrainTestSplit(imageDataShuffled, testFraction: 0.2);
 
-            model = pipeline.Fit(imageDataView);
+                var validationData =
+                    context.Transforms.Conversion.MapValueToKey("LabelKey", "Label", keyOrdinality: Microsoft.ML.Transforms.ValueToKeyMappingEstimator.KeyOrdinality.ByValue)
+                    .Append(context.Transforms.LoadRawImageBytes("Image", imagesFolder, "ImagePath"))
+                    .Fit(dataSets.TestSet)
+                    .Transform(dataSets.TestSet);
+
+                var imagesPipeline =
+                    context.Transforms.Conversion.MapValueToKey("LabelKey", "Label", keyOrdinality: Microsoft.ML.Transforms.ValueToKeyMappingEstimator.KeyOrdinality.ByValue)
+                    .Append(context.Transforms.LoadRawImageBytes("Image", imagesFolder, "ImagePath"));
+
+                var imageDataModel = imagesPipeline.Fit(dataSets.TrainSet);
+                var imageDataView = imageDataModel.Transform(dataSets.TrainSet);
+
+                var options = new ImageClassificationTrainer.Options()
+                {
+                    Arch = ImageClassificationTrainer.Architecture.ResnetV250,
+                    Epoch = 100,
+                    BatchSize = 20,
+                    LearningRate = 0.01f,
+                    LabelColumnName = "LabelKey",
+                    FeatureColumnName = "Image",
+                    ValidationSet = validationData
+                };
+
+                var pipeline = context.MulticlassClassification.Trainers.ImageClassification(options)
+                    .Append(context.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
+
+                Console.WriteLine($"Training Model...{cr}");
+
+                model = pipeline.Fit(imageDataView);
+
+                context.Model.Save(model, imageData.Schema, modelPath);
+                Console.WriteLine($"{cr}Model Saved...{cr}{modelPath}{cr}");
+            }
 
             var predictionEngine = context.Model.CreatePredictionEngine<ImageModelInput, ImagePrediction>(model);
+
             var testImagesFolder = Path.Combine(Environment.CurrentDirectory, "..", "..", "..", "test");
             var testFiles = Directory.GetFiles(testImagesFolder, "*", SearchOption.AllDirectories);
 
             var testImages = testFiles.Select(file => new ImageModelInput
             {
-                ImagePath = file
+                ImagePath = file,
+                Image = ImageToByteArray(Image.FromFile(file))
             });
 
-            Console.WriteLine($"{cr}");
+            // ---------------
 
-            var testImagesData = context.Data.LoadFromEnumerable(testImages);
-            var testImageDataView = imagesPipeline.Fit(testImagesData).Transform(testImagesData);
-            var predictions = model.Transform(testImageDataView);
-            var testPredictions = context.Data.CreateEnumerable<ImagePrediction>(predictions, reuseRowObject: false);
+            var line = new string('=', 50);
+            Console.WriteLine($"{cr}{line}{cr}");
 
-            foreach(var prediction in testPredictions)
+            foreach(var input in testImages)
             {
-                var labelIndex = prediction.PredictedLabel;
-                Console.WriteLine($"Image: {Path.GetFileName(prediction.ImagePath)}, Predicted Label: {prediction.PredictedLabel}");
+                var myPred = predictionEngine.Predict(input);
+                Console.WriteLine($"Image: {Path.GetFileName(myPred.ImagePath)} - Predicted Label: {myPred.PredictedLabel}");
             }
 
-            context.Model.Save(model, imageData.Schema, modelPath);
+            Console.WriteLine($"{cr}{line}{cr}");
+        }
 
-            Console.WriteLine($"{cr}Model Saved...{cr}{modelPath}{cr}");
+        // ------------------------------------------------
+
+        static byte[] ImageToByteArray(Image image)
+        {
+            using(MemoryStream stream = new MemoryStream())
+            {
+                image.Save(stream, System.Drawing.Imaging.ImageFormat.Jpeg);
+                return stream.ToArray();
+            }
         }
     }
 }
