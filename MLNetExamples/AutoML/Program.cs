08ce7f92b2ec857047bc9bbd20a26d0f517306c8
JoeArrow <joearrow1964@gmail.com>
2024-05-11 05:59:38 -0400

    Added Cats

--- MLNetExamples/AutoML/Program.cs
+++ MLNetExamples/AutoML/Program.cs
@@ -7,17 +7,13 @@ namespace AutoML
 {
     class Program
     {
+        public static readonly string cr = Environment.NewLine;
+
         static void Main(string[] args)
         {
             var context = new MLContext();
 
             var trainData = context.Data.LoadFromTextFile<HousingData>("./housing.csv", hasHeader: true, separatorChar: ',');
-
-            var settings = new RegressionExperimentSettings
-            {
-                MaxExperimentTimeInSeconds = 20,
-                OptimizingMetric = RegressionMetric.MeanAbsoluteError
-            };
 
             var labelColumnInfo = new ColumnInformation()
             {
@@ -32,15 +28,22 @@ namespace AutoML
                 }
             });
 
+            var settings = new RegressionExperimentSettings
+            {
+                MaxExperimentTimeInSeconds = 20,
+                OptimizingMetric = RegressionMetric.MeanAbsoluteError
+            };
+
             var experiment = context.Auto().CreateRegressionExperiment(settings);
 
             var result = experiment.Execute(trainData, labelColumnInfo, progressHandler: progress);
 
-            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine($"{cr}");
             Console.WriteLine("Best run:");
             Console.WriteLine($"Trainer name - {result.BestRun.TrainerName}");
             Console.WriteLine($"RSquared - {result.BestRun.ValidationMetrics.RSquared}");
             Console.WriteLine($"MAE - {result.BestRun.ValidationMetrics.MeanAbsoluteError}");
+            Console.WriteLine($"{cr}Done. Press Any Key...{cr}");
 
             Console.ReadLine();
         }
